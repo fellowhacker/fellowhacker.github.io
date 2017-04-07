11921689c3ce40327a13100ac69618ba78c8822e
Naveen Manikanta Kodigudla <naveenmanikanta111@gmail.com>
2017-04-07 15:08:01 +0530

    Update style.js

--- style.js
+++ style.js
@@ -3,3 +3,12 @@ var theta = $(window).scrollTop() / 100 % Math.PI;
 $('#leftgear').css({ transform: 'rotate(' + theta + 'rad)' });
 $('#rightgear').css({ transform: 'rotate(-' + theta + 'rad)' });
 });
+
+
+jQuery(document).ready(function(){
+	jQuery('.skillbar').each(function(){
+		jQuery(this).find('.skillbar-bar').animate({
+			width:jQuery(this).attr('data-percent')
+		},1000);
+	});
+});
